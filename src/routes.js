343a7abbac4e6215d63f57010da86ddfebbd2907
arllainm <arllain@gmail.com>
2020-01-31 06:40:13 -0300

    created the model's loader

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Arllain' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Arllain',
+    email: 'arllain@gmail.com',
+    password_hash: '123456',
+  });
+  return res.json(user);
+});
 
 export default routes;
