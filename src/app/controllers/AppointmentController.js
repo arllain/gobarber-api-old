f16901cef66a58f887a236e924e479430b4132ad
arllainm <arllain@gmail.com>
2020-02-08 07:29:06 -0300

    created service appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -35,54 +35,6 @@ class AppointmentController {
     });
     return res.json(appointment);
   }
-
-  async update(req, res) {
-    const schema = Yup.object().shape({
-      name: Yup.string(),
-      email: Yup.string().email(),
-      oldPassword: Yup.string().min(6),
-      password: Yup.string()
-        .min(6)
-        .when('oldPassword', (oldPassword, field) =>
-          oldPassword ? field.required() : field
-        ),
-      confirmPassword: Yup.string().when('password', (password, field) =>
-        password ? field.required().oneOf([Yup.ref('password')]) : field
-      ),
-    });
-
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation failed' });
-    }
-    const { email, oldPassword } = req.body;
-
-    const user = await Appointment.findByPk(req.userId);
-
-    if (!user) {
-      return res.status(400).json({ erro: 'User not found' });
-    }
-
-    if (email && email !== user.email) {
-      const userExists = await Appointment.findOne({ where: { email } });
-
-      if (userExists) {
-        return res.status(400).json({ erro: 'User already exists' });
-      }
-    }
-
-    if (oldPassword && !(await user.checkPassword(oldPassword))) {
-      return res.status(401).json({ erro: 'Password does not match' });
-    }
-
-    const { id, name, provider } = await user.update(req.body);
-
-    return res.json({
-      id,
-      name,
-      email,
-      provider,
-    });
-  }
 }
 
 export default new AppointmentController();
